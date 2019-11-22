6be69ddcdd28d26ca2eb540eec8fc41aa4ea8e30
Thomas Fay <thomas.fay@esss.se>
2019-11-22 17:26:08 +0100

    WIP

--- testAccess.sh
+++ testAccess.sh
@@ -8,36 +8,40 @@
 
 
 preamble="Class Name: "
+logFile='./logTest'
 
 function caput_test()
 {
   curVal=$(caget -t $1) 
   a=$(caput $1 $curVal 2>&1 > /dev/null)
-  [[ ${#a} -gt 0 ]] && echo "..CA write to $1: $a"
+  [[ ${#a} -gt 0 ]] && echo "..CA write to $1: $a" | tee -a "$logFile"
 }
 
 function pvput_test()
 {
   curVal=$(caget -t $1 2>&1) 
   a=$(pvput $1 $curVal 2>&1 >/dev/null)
-  [[ ${#a} -gt 0 ]] && echo "..write by PVA to $1: $a"
-
+  [[ ${#a} -gt 0 ]] && echo "..write by PVA to $1: $a" | tee -a "$logFile"
 }
+
+truncate -s 0 "$logFile"
+date > $logFile
+[[ ! -f "$logFile" ]] && echo "Failed to create log file in current directory, check write permissions."
 
 for PV in `cat ./listPV`
 do
-  echo -n "Testing PV: $PV ["
+  echo -n "Testing PV: $PV [" | tee -a "$logFile"
 
   recordType=$(caget $PV -d 38 2>&1 | grep 'Class Name:')
-  [[ $recordType == *"timed out"* ]] && echo "Couldn't find PV on network, exiting... " && exit
+  [[ $recordType == *"timed out"* ]] && echo "Couldn't find PV on network, exiting... " | tee -a "$logFile" && exit
 
-  [[ $recordType == *ai* ]] && recordType="ai" && echo "ai record]" && declare -a arrFields=(VAL HIGH HIHI LOW LOLO)
-  [[ $recordType == *ao* ]] && recordType="ao" && echo "ao record]" && declare -a arrFields=(VAL OROC DRVH DRVL HIGH HIHI LOW LOLO)
-  [[ $recordType == *bi* ]] && recordType="ai" && echo "bi record]" && declare -a arrFields=(VAL)
-  [[ $recordType == *bo* ]] && recordType="ao" && echo "bo record]" && declare -a arrFields=(VAL)
+  [[ $recordType == *ai* ]] && recordType="ai" && echo "ai record]" | tee -a "$logFile" && declare -a arrFields=(VAL HIGH HIHI LOW LOLO)
+  [[ $recordType == *ao* ]] && recordType="ao" && echo "ao record]" | tee -a "$logFile" && declare -a arrFields=(VAL OROC DRVH DRVL HIGH HIHI LOW LOLO)
+  [[ $recordType == *bi* ]] && recordType="ai" && echo "bi record]" | tee -a "$logFile" && declare -a arrFields=(VAL)
+  [[ $recordType == *bo* ]] && recordType="ao" && echo "bo record]" | tee -a "$logFile" && declare -a arrFields=(VAL)
   # No write checking required for strings or waveforms.
-  [[ $recordType == *string* ]] && recordType="" && echo "..skipping string PV]" && declare -a arrFields=()
-  [[ $recordType == *waveform* ]] && recordType="" && echo "..skipping waveform PV]" && declare -a arrFields=()
+  [[ $recordType == *string* ]] && recordType="" && echo "..skipping string PV]" | tee -a "$logFile" && declare -a arrFields=()
+  [[ $recordType == *waveform* ]] && recordType="" && echo "..skipping waveform PV]" | tee -a "$logFile" && declare -a arrFields=()
   
 
   # Test if remote write is trapped correctly for caput and pvput
